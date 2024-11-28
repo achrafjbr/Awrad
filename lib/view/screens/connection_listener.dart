b0af2b39f672f515894fb1abc55611f331ded824
Achraf El jabbar <165720727+achrafjbr@users.noreply.github.com>
2024-11-28 11:49:11 +0100

    - Add connection behaviour for the whole app, I implemented connection_cubit.dart, and connection_listener.dart for that purpose

---
+++ lib/view/screens/connection_listener.dart
@@ -0,0 +1,31 @@
+import 'package:abu_sandia/controller/cubits/internet_cubit/connection_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ConnectionListener extends StatelessWidget {
+  final Widget child;
+
+  const ConnectionListener({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<ConnectionCubit, ConnectionStatus>(
+      listener: (context, state) {
+        if (state  == ConnectionStatus.disconnected) {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => AlertDialog(
+              title: Text("No Internet Connection"),
+              content: Text("Please check your internet connection."),
+              actions: [],
+            ),
+          );
+        } else {
+          Navigator.of(context).pop(); // Close the dialog if connection returns
+        }
+      },
+      child: child,
+    );
+  }
+}
